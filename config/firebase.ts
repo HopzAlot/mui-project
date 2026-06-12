297d2130ae78c610fbba614bc94e6f7b96cda649
HopzAlot <rehansaqib2006@gmail.com>
2026-06-12 15:56:39 +0500

    fixed a little bug.

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,9 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import dotenv from "dotenv";
-
-dotenv.config();
+require("dotenv").config();
 
 const firebaseConfig = {
   apiKey: process.env.VITE_FIREBASE_API_KEY,
